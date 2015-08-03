c0e2552df8b72ad1357317fd397882e4f0f0ab46
brit13-meet <brit13@meet.mit.edu>
2015-08-03 11:49:01 +0300

    filtering is now working

--- project/app/views.py
+++ project/app/views.py
@@ -1,96 +1,101 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.utils.datastructures import MultiValueDictKeyError
-from models import Account, User
+from models import Account, User, Event
 import datetime
-
 
 
 # Create your views here.
 
 def home(request):
-	dictionary = {"active" : "homeTab" }
-	return render(request, 'app/home.html', dictionary)
+    dictionary = {"active": "homeTab"}
+    return render(request, 'app/home.html', dictionary)
+
 
 def login_page(request):
-	dictionary = {"active" : "LoginTab" }
-	return render(request, 'app/login.html', dictionary)
+    dictionary = {"active": "LoginTab"}
+    return render(request, 'app/login.html', dictionary)
+
 
 def login2(request):
-	dictionary = {"active" : "LoginTab" }
-	return render(request, 'app/Events.html', dictionary)
+    dictionary = {"active": "LoginTab"}
+    return render(request, 'app/Events.html', dictionary)
+
 
 def signup(request):
-	dictionary = {"active" : "registerTab" }
-	return render(request, 'app/signup.html', dictionary)
+    dictionary = {"active": "registerTab"}
+    return render(request, 'app/signup.html', dictionary)
+
 
 def aboutus(request):
-	dictionary = {"active" : "aboutUsTab" }
-	return render(request, 'app/aboutus.html', dictionary)
+    dictionary = {"active": "aboutUsTab"}
+    return render(request, 'app/aboutus.html', dictionary)
 
 
 def events(request):
-	dictionary = {"active" : "eventsTab" }
-	return render(request, 'app/Events.html', dictionary)
+    dictionary = {"active": "eventsTab"}
+    dictionary["events"] = Event.objects.all()
+    return render(request, 'app/Events.html', dictionary)
 
 
 def volunteam(request):
-	dictionary = {"active" : "VolunTeamTab" }
-	return render(request, 'app/volunteam.html', dictionary)
+    dictionary = {"active": "VolunTeamTab"}
+    return render(request, 'app/volunteam.html', dictionary)
+
 
 def signupRequest(request):
-	
-	try:
-		firstName = request.POST['firstName']
-		lastName = request.POST['lastName']
-		password = request.POST['password']
-		email = request.POST['emailAddress']
-		birthdayDay = request.POST['birthdayDay']
-		birthdayMonth = request.POST['birthdayMonth']
-		birthdayYear = request.POST['birthdayYear']
-		nationality = request.POST['nationality']
-	except MultiValueDictKeyError:
-		return HttpResponse("Not all of the fields were filled")
-	
 
-	if (firstName == "" or lastName == "" or password == "" or email == "" or birthdayDay == "" or birthdayMonth == "" or birthdayYear == "" or nationality == ""):
-		return HttpResponse("Some of the fields are empty.")
+    try:
+        firstName = request.POST['firstName']
+        lastName = request.POST['lastName']
+        password = request.POST['password']
+        email = request.POST['emailAddress']
+        birthdayDay = request.POST['birthdayDay']
+        birthdayMonth = request.POST['birthdayMonth']
+        birthdayYear = request.POST['birthdayYear']
+        nationality = request.POST['nationality']
+    except MultiValueDictKeyError:
+        return HttpResponse("Not all of the fields were filled")
 
-	if (nationality == "isra"):
-		nationality = False
-	else:
-		nationality = True
+    if (firstName == "" or lastName == "" or password == "" or email == "" or birthdayDay == "" or birthdayMonth == "" or birthdayYear == "" or nationality == ""):
+        return HttpResponse("Some of the fields are empty.")
 
-	birthday = datetime.datetime(int(birthdayYear), int(birthdayMonth), int(birthdayDay))
-	# birthday = datetime.datetime.now()
-	user_obj = User(first_name = firstName, last_name = lastName, email = email, password = password, username = email)
-	user_obj.save()
+    if (nationality == "isra"):
+        nationality = False
+    else:
+        nationality = True
 
-	account_obj = Account(user = user_obj, isPalestinian = nationality)
-	account_obj.save()
+    birthday = datetime.datetime(
+        int(birthdayYear), int(birthdayMonth), int(birthdayDay))
+    # birthday = datetime.datetime.now()
+    user_obj = User(first_name=firstName, last_name=lastName,
+                    email=email, password=password, username=email)
+    user_obj.save()
 
-	return HttpResponse("Banana")
+    account_obj = Account(user=user_obj, isPalestinian=nationality)
+    account_obj.save()
+
+    return HttpResponse("Banana")
+
+
 def loginRequest(request):
-	try:
-		emailAddress = request.POST['emailAddress']
-		password = request.POST['password']
-	except MultiValueDictKeyError:
-		return HttpResponse("Not all of the fields were filled")
-	user = authenticate(username=emailAddress, password=password)
-	if user is not None:
-	    # the password verified for the user
-	    if user.is_active:
-		login(request, user)
-		return redirect("/events")
+    try:
+        emailAddress = request.POST['emailAddress']
+        password = request.POST['password']
+    except MultiValueDictKeyError:
+        return HttpResponse("Not all of the fields were filled")
+    user = authenticate(username=emailAddress, password=password)
+    if user is not None:
+        # the password verified for the user
+        if user.is_active:
+            login(request, user)
+            return redirect("/events")
 
+        else:
+            return HttpResponse("The password is valid, but the account has been disabled!")
+    else:
 
-	    else:
-		return HttpResponse("The password is valid, but the account has been disabled!")
-	else:
-		
-		return redirect("/volunteam/")
-	
+        return redirect("/volunteam/")
 
 
 def showUsers(request):
-	return render(request, 'app/showUsers.html', {"Accounts": Account.objects.all()})
-
+    return render(request, 'app/showUsers.html', {"Accounts": Account.objects.all()})
