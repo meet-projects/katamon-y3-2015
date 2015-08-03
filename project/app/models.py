6560992d4cf3f258145248953853c16899bc931d
brit13-meet <brit13@meet.mit.edu>
2015-08-03 17:03:44 +0300

    fixed register, started working on auto loading events

--- project/app/models.py
+++ project/app/models.py
@@ -3,38 +3,61 @@ from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
+
+
 class Account(models.Model):
-	isPalestinian = models.BooleanField()
-	#birthday = models.DateTimeField()
-	user = models.OneToOneField(User)
-	def __unicode__(self):
-		return self.user.username + " " + self.user.email + " " + str(self.birthday)
+    isPalestinian = models.BooleanField()
+    #birthday = models.DateTimeField()
+    user = models.OneToOneField(User)
 
-#Event model
+    def __unicode__(self):
+        return self.user.username + " " + self.user.email + " " + str(self.birthday)
+
+# Event model
+
+
 class Event(models.Model):
-	name = models.CharField(max_length=30)
-	date = models.DateTimeField()
-	time = models.CharField(max_length=30)
-	address = models.CharField(max_length=60)
-	description = models.CharField(max_length=300)
-	accounts = models.ManyToManyField(Account)
-	#attendees
+    name = models.CharField(max_length=30)
+    date = models.DateTimeField()
+    time = models.CharField(max_length=30)
+    address = models.CharField(max_length=60)
+    description = models.CharField(max_length=300)
+    accounts = models.ManyToManyField(Account)
 
-#AddEvent model
+    def getHtmlClasses(self):
+        dayClasses = ["monday-day", "tuesday-day", "wednesday-day",
+                      "thursday-day", "friday-day", "saturday-day"]
+        returnList = []
+        returnList.append(dayClasses[self.date.weekday()])
+        if self.time < 12:
+            returnList.append("morning-time")
+        elif self.time < 14:
+            returnList.append("noon-time")
+        elif self.time < 16:
+            returnList.append("afternoon-time")
+        elif self.time < 18:
+            returnList.append("evening-time")
+    # attendees
+
+        returnList.append("handicapped-field")
+# AddEvent model
+
+
 class AddEvent(models.Model):
-	name = models.CharField(max_length=30)
-	description = models.CharField(max_length=300)
-	location = models.CharField(max_length=60)
-	time = models.CharField(max_length=30)
-	date = models.DateTimeField()
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    location = models.CharField(max_length=60)
+    time = models.CharField(max_length=30)
+    date = models.DateTimeField()
 
-#Org model
+# Org model
+
+
 class Organization(models.Model):
-	name = models.CharField(max_length=30)
-	number = models.CharField(max_length=15)
-	address = models.CharField(max_length=60)
-	description = models.CharField(max_length=300)
-	event = models.ManyToManyField(Event)
-	#logo = models.FileField(upload_to='app/static/app/img')
-	website = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=15)
+    address = models.CharField(max_length=60)
+    description = models.CharField(max_length=300)
+    event = models.ManyToManyField(Event)
+    #logo = models.FileField(upload_to='app/static/app/img')
+    website = models.CharField(max_length=100)
--- project/app/views.py
+++ project/app/views.py
@@ -20,18 +20,19 @@ def login_page(request):
 
 
 def login2(request):
-	dictionary = {"active" : "LoginTab" }
-	try:
-		email = request.POST['emailAddress']
-		password=request.POST['password']
-	except MultiValueDictKeyError:
-		return redirect("/login")
-	return login_request(request, email, password)
-		
+    dictionary = {"active": "LoginTab"}
+    try:
+        email = request.POST['emailAddress']
+        password = request.POST['password']
+    except MultiValueDictKeyError:
+        return redirect("/login")
+    return login_request(request, email, password)
+
+
 def logoutrequest(request):
-	dictionary = {"active" : "LogOutTab" }
-	logout(request)
-	return redirect("/")
+    dictionary = {"active": "LogOutTab"}
+    logout(request)
+    return redirect("/")
 
 
 def signup(request):
@@ -40,8 +41,9 @@ def signup(request):
 
 
 def addevent(request):
-	dictionary = {"active" : "registerTab" }
-	return render(request, 'app/addevent.html', dictionary)
+    dictionary = {"active": "registerTab"}
+    return render(request, 'app/addevent.html', dictionary)
+
 
 def aboutus(request):
     dictionary = {"active": "aboutUsTab"}
@@ -65,49 +67,62 @@ def OrgSignUp(request):
 
 
 def signupRequest(request):
-	try:
-		firstName = request.POST['firstName']
-		lastName = request.POST['lastName']
-		password = request.POST['password']
-		email = request.POST['emailAddress']
-		birthdayDay = request.POST['birthdayDay']
-		birthdayMonth = request.POST['birthdayMonth']
-		birthdayYear = request.POST['birthdayYear']
-		nationality = request.POST['nationality']
-	except MultiValueDictKeyError:
-		return HttpResponse("Not all of the fields were filled")
-	
+    dictionary = {"active": "registerTab"}
 
-	if (firstName == "" or lastName == "" or password == "" or email == "" or birthdayDay == "" or birthdayMonth == "" or birthdayYear == "" or nationality == ""):
-		return HttpResponse("Some of the fields are empty.")
-	
-	if (User.objects.filter(email=email).count()!=0):
-		return login_request(request, email, password)
-		
-	if (nationality == "isra"):
-		nationality = False
-	else:
-		nationality = True
+    firstName = request.POST.get('firstName')
+    lastName = request.POST.get('lastName')
+    password = request.POST.get('password')
+    email = request.POST.get('emailAddress')
+    birthdayDay = request.POST.get('birthdayDay')
+    birthdayMonth = request.POST.get('birthdayMonth')
+    birthdayYear = request.POST.get('birthdayYear')
+    nationality = request.POST.get('nationality')
 
-	birthday = datetime.datetime(int(birthdayYear), int(birthdayMonth), int(birthdayDay))
-	# birthday = datetime.datetime.now()
-	user_obj = User(first_name = firstName, last_name = lastName, email = email, username = email)
-	user_obj.set_password(password)
-	user_obj.save()
+    if firstName is None \
+            or \
+            lastName is None or \
+            password is None or \
+            email is None or \
+            birthdayDay is None or \
+            birthdayMonth is None or \
+            birthdayYear is None or nationality is None:
+        dictionary["errors"] = ["Some of the fields are empty."]
+        return render(request, 'app/signup.html', dictionary)
 
-	account_obj = Account(user = user_obj, isPalestinian = nationality)
-	account_obj.save()
-	return login_request(request, email, password)
+    if (User.objects.filter(email=email).count() != 0):
+        dictionary["errors"] = [
+            "Email Address already exists, redirected to login page"]
+        return render(request, 'app/signup.html', dictionary)
+
+    if (nationality == "isra"):
+        nationality = False
+    else:
+        nationality = True
+
+    birthday = datetime.datetime(
+        int(birthdayYear), int(birthdayMonth), int(birthdayDay))
+    # birthday = datetime.datetime.now()
+    user_obj = User(
+        first_name=firstName, last_name=lastName, email=email, username=email)
+    user_obj.set_password(password)
+    user_obj.save()
+
+    account_obj = Account(user=user_obj, isPalestinian=nationality)
+    account_obj.save()
+    return login_request(request, email, password)
+
 
 def login_request(request, email, password):
-	user = authenticate(username=email, password=password)
-	if user is not None:
-		#user and password is correct
-		login(request, user)
-		return redirect("/events")
-	else:
-    	# the authentication system was unable to verify the username and password
-		return redirect("/login")
+    user = authenticate(username=email, password=password)
+    if user is not None:
+        #user and password is correct
+        login(request, user)
+        return redirect("/events")
+    else:
+        # the authentication system was unable to verify the username and
+        # password
+        return redirect("/login")
+
 
 def showUsers(request):
     return render(request, 'app/showUsers.html', {"Accounts": Account.objects.all()})
